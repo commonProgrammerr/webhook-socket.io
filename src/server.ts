b426b5ec01c459ac7517babb421cb046b6bb1f1b
Andre Escorel <agver@ecomp.poli.br>
2021-12-13 13:29:57 -0300

    'merge' requests controler and server file from 'dev_only_socket_webhook'

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express, { Request } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-// import AlertsController from './controllers/alerts';
-// import AlertView from './views/alert_view';
 
 import RequestController from './controllers/requests'
-import './database/connection';
+import UsersController from './controllers/users'
 
 const app = express();
 const httpServer = createServer(app);
@@ -18,19 +16,18 @@ app.use(express.json());
 
 const port = process.env.PORT || 3030;
 
+const feed = []
+
 io.on('connection', async (socket) => {
   console.log(socket.id, ' chegou')
-  socket.on('disconnect', () => console.log(socket.id, ' leave...'))  
+  socket.on('disconnect', () => console.log(socket.id, ' leave...'))
 })
 
-// app.post('/alert', AlertsController.alert(io));
-
-// app.get('/alerts', AlertsController.index)
-
-app.get('/feed', RequestController.feed)
-app.post('/request/create', RequestController.pop(io))
-app.get('/request', RequestController.find)
-
-
+app.post('/requests/create', RequestController.push(io))
+app.post('/requests/feed', RequestController.feed_controller)
+app.post('/requests/search', RequestController.find)
+app.post('/requests/report', RequestController.report(io))
+app.post('/requests/suport', RequestController.require_suport(io))
+app.post('/auth', UsersController.auth)
 
 httpServer.listen(port, () => console.log(`Server running at http://localhost:${port}/`));
