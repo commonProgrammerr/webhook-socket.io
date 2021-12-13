80b81d323e38044813f170b130b68a10e727b4e8
Andre Escorel <agver@ecomp.poli.br>
2021-12-13 13:29:57 -0300

    'merge' requests controler and server file from 'dev_only_socket_webhook'

--- src/controllers/requests.ts
+++ src/controllers/requests.ts
@@ -1,110 +1,33 @@
-import assert from 'node:assert';
-import { Server } from 'socket.io'
+import assert, { AssertionError } from 'assert';
+import { Server } from 'socket.io';
 import { Request, Response } from 'express';
-import { FindOneOptions, getRepository } from 'typeorm';
+import { uuid } from 'uuidv4';
 
-import RequestModel from '../models/Request'
-import RequestView from '../views/requests_view'
-import { RequestsTypes } from './../models/Request'
+let feed = [] as Array<{
+  [key: string]: any;
+}>;
 
-
-export async function handleCreate(data: Omit<RequestModel, 'id' | 'created_at' | 'updated_at'>) {
-  console.log("Criando novo request");
-  console.log(data)
-  const requestRepository = getRepository(RequestModel);
-
-  const request = requestRepository.create({ ...data });
-
-  return requestRepository.save(request);
-}
+const reports = [] as Array<{
+  oc_id: string;
+  usr_id: string;
+  tool: string;
+  type: string;
+  type_obs?: string;
+}>;
 
 export default {
-  async index(_req: Request, res: Response) {
-    const requestRepository = getRepository(RequestModel);
-
-    const requests = await requestRepository.find();
-
-    return res.json(RequestView.renderMany(requests));
-  },
-
-  async find(req: Request, res: Response) {
-    const id = String(req.query.id)
-
-    console.log('search for ', { id })
-    try {
-      const requestRepository = getRepository(RequestModel);
-
-      const request_entity = await requestRepository.findOneOrFail({
-        where: { id }
-      })
-
-      if (!request_entity)
-        return res.status(404).json({
-          message: "Inexistent entity"
-        })
-
-      return res.status(200).json(request_entity)
-    } catch (error) {
-      return res.status(500).json({
-        error: (error as Error)?.name,
-        message: (error as Error)?.message
-      })
-    }
-  },
-
-  async create(req: Request, res: Response) {
-    try {
-      const created_response = await handleCreate(req.body)
-
-      return res.status(201).json(created_response)
-
-    } catch (error) {
-      return res.status(400).json({
-        error: (error as Error)?.name,
-        message: (error as Error)?.message
-      })
-    }
-  },
-
-  async feed(req: Request, res: Response) {
-    const page = Number(req.query.page)
-    const per_page = 5
-    const offset = (page ? Number(page) - 1 : 0) * per_page
-    console.log({
-      page,
-      offset
-    })
-
-    const requestRepository = getRepository(RequestModel);
-
-    const requests = await requestRepository.find({
-      select: ['id', 'local', 'box', 'piso', 'type'],
-      order: {
-        created_at: 'DESC'
-      },
-      skip: offset,
-      take: per_page,
-      where: [
-        { type: RequestsTypes.AJUDA },
-        { type: RequestsTypes.ENTUPIMENTO }
-      ]
-    });
-
-    return res.json(RequestView.feed(requests));
-  },
-
-  pop(io: Server) {
+  push(io: Server) {
     return async (req: Request, res: Response) => {
-      console.log('new request')
       try {
-        const new_occurrence = await handleCreate(req.body)
-        const {
-          id,
-          box,
-          local,
-          piso,
-          type
-        } = new_occurrence
+        const { id, zone_id, box, local, piso, type } = req.body;
+        console.log('new request ', id);
+
+        assert(id, `Invalid body! Missing the 'id' field.`);
+        assert(zone_id, `Invalid body! Missing the 'zone_id' field.`);
+        assert(box, `Invalid body! Missing the 'box' field.`);
+        assert(local, `Invalid body! Missing the 'local' field.`);
+        assert(piso, `Invalid body! Missing the 'piso' field.`);
+        assert(type, `Invalid body! Missing the 'type' field.`);
 
         assert(
           io.emit('update-feed:new_request', {
@@ -112,19 +35,131 @@ export default {
             box,
             local,
             piso,
-            type
+            type,
           }),
-          'Socket connection fail!'
-        )
-
-        return res.status(201).json(new_occurrence)
-
+          new Error('Socket connection fail!')
+        );
+        feed.push(req.body);
+        return res.status(201).send();
       } catch (error) {
-        return res.status(400).json({
-          error: (error as Error)?.name,
-          message: (error as Error)?.message
-        })
+        if (error instanceof AssertionError) {
+          return res.status(400).json({
+            code: 'Missing Field',
+            message: error.message,
+          });
+        } else {
+          return res.status(500).json({
+            code: (error as Error)?.name,
+            message: (error as Error)?.message,
+          });
+        }
+      }
+    };
+  },
+
+  feed_controller: async (req: Request, res: Response) => {
+    return res.status(200).json({
+      feed,
+    });
+  },
+
+  find: async (req: Request, res: Response) => {
+    const { oc_id } = req.body;
+    try {
+      const result = feed.find((item) => item?.id === oc_id);
+      assert(!!result, 'Id invalido ou inexistente');
+
+      return res.status(200).json();
+    } catch (error) {
+      if (error instanceof AssertionError) {
+        return res.status(404).json({
+          code: 'Not found!',
+          message: error.message,
+        });
+      } else {
+        return res.status(500).json({
+          code: (error as Error)?.name,
+          message: (error as Error)?.message,
+        });
       }
     }
-  }
+  },
+
+  report(io: Server) {
+    return async (req: Request, res: Response) => {
+      try {
+        const { oc_id, usr_id, tool, type, type_obs } = req.body;
+        feed = feed.filter((item) => item.id !== oc_id);
+        reports.push({ oc_id, usr_id, tool, type, type_obs });
+        
+        assert(
+          io.emit('update-feed:remove', {
+            id: oc_id,
+          }),
+          new Error('Socket connection fail!')
+        );
+
+        return res.status(200).json({
+          usr_id,
+          oc_id,
+        });
+      } catch (error) {
+        if (error instanceof AssertionError) {
+          return res.status(400).json({
+            code: 'Missing Field',
+            message: error.message,
+          });
+        } else {
+          return res.status(500).json({
+            code: (error as Error)?.name,
+            message: (error as Error)?.message,
+          });
+        }
+      }
+    };
+  },
+
+  require_suport(io: Server) {
+    return (req: Request, res: Response) => {
+      const { oc_id, usr_id, apoio, description } = req.body;
+
+      try {
+        const item = feed.find((item) => item.id === oc_id);
+        assert(item, 'Ocorrencia não encontrada!');
+        const { box, local, piso } = item;
+        const id = uuid();
+        const full_description = `${apoio}:\n${description}`;
+        
+        feed.push({
+          ...item,
+          id,
+          description: full_description,
+        });
+
+        assert(
+          io.emit('update-feed:new_request', {
+            id,
+            box,
+            local,
+            piso,
+            type: 2,
+          }),
+          new Error('Socket connection fail!')
+        );
+        return res.status(201).send();
+      } catch (error) {
+        if (error instanceof AssertionError) {
+          return res.status(404).json({
+            code: 'Ocurrence not found',
+            message: error.message,
+          });
+        } else {
+          return res.status(500).json({
+            code: (error as Error)?.name,
+            message: (error as Error)?.message,
+          });
+        }
+      }
+    };
+  },
 };
