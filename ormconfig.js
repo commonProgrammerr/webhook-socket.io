03e2793497932fbb31c6d74bc4c6fab4417137be
Andre Escorel <agver@ecomp.poli.br>
2022-08-11 00:04:02 -0300

    BROKEN: changin database

--- ormconfig.js
+++ ormconfig.js
@@ -1,44 +1,34 @@
 const path = require('path');
 // const { createConnection } = require('typeorm');
 
-const isProd = process.env.NODE_ENV === 'prod';
+const isProd =
+  process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production';
 const basePath = isProd ? './build/src' : './src';
 const filesTypes = isProd ? '*.js' : '*.ts';
-const DATABASE_URL = process.env.DATABASE_URL || 'localhost';
-console.log(DATABASE_URL);
-// const url$ = !!process.env.DATABASE_URL;
 
-const baseConfig = {
-  // type: 'postgres',
-  // database: isProd ? 'maint_app' : 'dev_maint_app',
-  migrations: [path.resolve(basePath, 'database/migrations', filesTypes)],
-  entities: [path.resolve(basePath, 'models', filesTypes)],
-  cli: {
-    migrationsDir: path.resolve(basePath, 'database/migrations'),
-  },
-};
+const url = isProd
+  ? process.env.DATABASE_URL
+  : 'mysql://mimodev:Arer3366547@10@mimodev.mysql.dbaas.com.br:3306/mimodev';
+// `postgres://postgres:postgres@localhost:5432/dev_maint_app`;
+
+console.log('connecting to', url);
 module.exports = {
-  // type: 'sqlite',
-  // database: path.resolve(basePath, 'database/database.sqlite'),
-  type: 'postgres',
-  database: isProd ? 'maint_app' : 'dev_maint_app',
+  type: 'mysql',
   migrations: [path.resolve(basePath, 'database/migrations', filesTypes)],
   entities: [path.resolve(basePath, 'models', filesTypes)],
   cli: {
     migrationsDir: path.resolve(basePath, 'database/migrations'),
+    entitiesDir: path.resolve(basePath, 'models'),
   },
-  // host: isProd ? BASE_URL : 'localhost',
-  url: isProd
-    ? DATABASE_URL
-    : `postgres://maint:maint@localhost:5432/dev_maint_app`,
+  url,
   extra: isProd
     ? {
-        ssl: true,
+        ssl: {
+          ca: process.env.SSL_CERT,
+          rejectUnauthorized: false,
+        },
       }
     : undefined,
   synchronize: false,
-  logging: true,
-  // port: 5432,
-  // username: 'maint',
-  // password: 'maint',
+  logging: isProd,
 };
--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,10 +1,13 @@
+import { MinLength } from 'class-validator';
 import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  BaseEntity,
 } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 export enum EventType {
   REPARO = 1,
@@ -14,48 +17,59 @@ export enum EventType {
 }
 
 @Entity('events')
-export default class Event {
+export default class Event extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('text')
-  zone_id: string;
+  @Column({ type: 'text', nullable: true })
+  description?: string | null;
 
   @Column({ type: 'boolean', nullable: true })
   enable?: boolean | null;
 
   @Column('integer')
+  @MinLength(1)
+  zone_id: number;
+
+  @Column('integer')
   type: EventType;
 
-  @Column({ type: 'text' })
+  @Column('text')
+  @MinLength(1)
   local: string;
 
-  @Column({ type: 'text' })
+  @Column('text')
+  @MinLength(1)
   piso: string;
 
   @Column({ type: 'text', nullable: true })
   box?: string | null;
 
-  @Column({ type: 'text' })
+  @Column('text')
+  @MinLength(1)
   banheiro: string;
 
   @Column({ type: 'text', nullable: true })
   mac?: string | null;
 
-  @Column({ type: 'text', nullable: true })
-  description?: string | null;
+
+  @Column({ type: 'integer', nullable: true })
+  relatorio_id?: number;
+
+  @Column({ type: 'integer', nullable: true })
+  compleated_by?: number;
+
+  @Column({ type: 'integer', nullable: true })
+  request_by?: number;
+
+  @Column({ type: 'timestamp', nullable: true })
+  inicio?: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  fim?: Date;
 
   @Column({ type: 'text', nullable: true })
   payload?: string | null;
-
-  @Column({ type: 'text', nullable: true })
-  suport_type?: string | null;
-
-  @Column({ type: 'text', nullable: true })
-  local_photo?: string | null;
-
-  @Column({ type: 'text', nullable: true })
-  tool?: string | null;
 
   @CreateDateColumn()
   created_at: Date;
